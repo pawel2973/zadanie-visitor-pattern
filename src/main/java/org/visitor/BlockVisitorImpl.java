16fd437964e55d6c01195d8bd40dbfa8eb84ea7b
pawel2973 <37537809+pawel2973@users.noreply.github.com>
2023-02-11 18:56:03 +0100

    Visitor impl refactored
    Separated the visit method into two separate methods for each block type (Block, CompositeBlock).

--- src/main/java/org/visitor/BlockVisitorImpl.java
+++ src/main/java/org/visitor/BlockVisitorImpl.java
@@ -10,21 +10,23 @@ public class BlockVisitorImpl implements Visitor {
 
     @Override
     public Stream<Block> visit(Block block) {
-
-        if (block instanceof CompositeBlock) {
-            CompositeBlock compositeBlock = (CompositeBlock) block;
-            List<Block> childBlocks = compositeBlock.getBlocks();
-
-            if (childBlocks == null) {
-                return Stream.of(compositeBlock);
-            }
-
-            return Stream.concat(
-                    Stream.of(compositeBlock),
-                    childBlocks.stream()
-                            .flatMap(this::visit));
-        }
-
         return Stream.of(block);
     }
+
+    @Override
+    public Stream<Block> visit(CompositeBlock block) {
+        List<Block> childBlocks = block.getBlocks();
+
+        if (childBlocks == null) {
+            return Stream.of(block);
+        }
+
+        return Stream.concat(
+                Stream.of(block),
+                childBlocks.stream()
+                        .peek(x-> System.out.println(this))
+                        .flatMap(cBlock -> cBlock.accept(this)));
+    }
 }
+
+
--- src/main/java/org/visitor/Visitor.java
+++ src/main/java/org/visitor/Visitor.java
@@ -1,10 +1,12 @@
 package org.visitor;
 
 import org.wall.Block;
+import org.wall.CompositeBlock;
 
 import java.util.stream.Stream;
 
 public interface Visitor {
 
     Stream<Block> visit(Block block);
+    Stream<Block> visit(CompositeBlock block);
 }
